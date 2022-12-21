a1eb1a45171c841b45482be37b19532c60663c6a
unknown <deianandrei22@yahoo.com>
2022-12-21 23:26:20 +0200

    Added Home Buttons

--- fotbaltm/src/navigation/index.js
+++ fotbaltm/src/navigation/index.js
@@ -4,6 +4,9 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeLogin from "../screens/HomeLogin";
 import Register from "../screens/Register";
 import Homescreen from "../screens/HomeScreen";
+import BookField from "../screens/BookField";
+import SeeYourBooks from "../screens/SeeBooks";
+import FindTeamMates from "../screens/FindTeamMates";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +17,9 @@ function Navigation() {
                 <Stack.Screen name="HomeLogin" component={HomeLogin}/>
                 <Stack.Screen name="Register" component={Register} />
                 <Stack.Screen name="Home" component={Homescreen} />
+                <Stack.Screen name="BookField" component={BookField} />
+                <Stack.Screen name="SeeBooks" component={SeeYourBooks} />
+                <Stack.Screen name="FindTeamMates" component={FindTeamMates} />
             </Stack.Navigator>
         </NavigationContainer>
     );
