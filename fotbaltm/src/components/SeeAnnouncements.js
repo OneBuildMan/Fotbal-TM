2c6420ec5cd55eed156824c4b0ff7235e0cb36b3
adi0300 <adiciatlosi@yahoo.com>
2023-01-05 21:25:19 +0200

    join announcement functionality with firestore

--- fotbaltm/src/components/SeeAnnouncements.js
+++ fotbaltm/src/components/SeeAnnouncements.js
@@ -1,19 +1,42 @@
 import React, {useState, useEffect} from "react";
-import { Text, View, Image, StyleSheet, useWindowDimensions, ScrollView, TouchableOpacity} from "react-native";
+import { Text, View, Image, StyleSheet, useWindowDimensions, ScrollView, TouchableOpacity, Alert} from "react-native";
 import Button from "./Button";
+import firestore from "@react-native-firebase/firestore";
 
 function SeeAnnouncements() {
-    const announcements = [{id:"2144", date: "05.06", time: "20:00", field: "Friends Arena", occupiedPlaces: 10},
-                           {id:"2134" ,date: "06.06", time: "18:00", field:"Banu Sport", occupiedPlaces: 8}]       // de forma
+    const [announcements, setAnnouncements] = useState([]);
     const [expanded, setExpanded] = useState(null);
+
+    useEffect( () => {
+        firestore().collection('announcements').onSnapshot(snapshot => {
+            let items = snapshot.docs.map(doc => doc.data());
+            const items_ids = snapshot.docs.map(doc => doc.id);
+            let counter = 0;
+            items.forEach( (it) => {
+                it.id = items_ids[counter];
+                counter++;
+            })
+            counter = 0;
+
+            let announcementsNonOccupied = items.filter( it => it.occupiedPlaces<12); // o sa fie aratate doar anunturile care nu sunt deja ocupate
+            setAnnouncements(announcementsNonOccupied);
+        })
+    }, []);
 
     const toggleExpand = id => {
         setExpanded(expanded === id ? null : id);
       };
 
-    const onJoinAnnouncementPressed = () => {
+    const onJoinAnnouncementPressed = (id, places) => {
         // locurile ocupate vor fi incrementate cu 1 si salvate in database. Daca se ajunge la 12 locuri ocupate, anuntul nu 
         // va mai aparea in aceasta lista.
+        const incrementedPlaces = parseInt(places) + 1;
+        firestore().collection('announcements').doc(id).update({ occupiedPlaces: incrementedPlaces})
+        .then(() =>  Alert.alert('Te-ai alaturat anuntului cu succes'))
+        .catch((err) => {
+            Alert.alert('A aparut o eroare');
+            console.log(err);
+        })
     }
 
     return(
@@ -25,7 +48,7 @@ function SeeAnnouncements() {
                         <View style={styles.details}>
                             <Text style={styles.label}>{item.field}</Text>
                             <Text style={styles.label}>{item.occupiedPlaces}/12 locuri ocupate</Text>
-                            <Button text="Alatura-te" bgColor="white" width="60%" onPress={onJoinAnnouncementPressed}></Button>
+                            <Button text="Alatura-te" bgColor="white" width="60%" onPress={() => onJoinAnnouncementPressed(item.id, item.occupiedPlaces)}></Button>
                         </View>
                     )}
                 </TouchableOpacity>      
